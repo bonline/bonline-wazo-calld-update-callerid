f1420c14ed1955955858c2f705aea72875270997
Christie Grinham <christiegrinham@gmail.com>
2026-02-05 15:17:07 +0000

    Support modes

--- bonline_wazo_calld_update_callerid/services.py
+++ bonline_wazo_calld_update_callerid/services.py
@@ -3,15 +3,36 @@ class UpdateCallerIDService:
         self._ari = ari
 
     def update_caller_id(self, call_id, request_data):
-        """ Use CALLERID to update the caller id shown to the callee
-        https://www.voip-info.org/asterisk-func-callerid/
+        """ Updates the CALLERID seen by the callee.
+        Supports: overwrite, prepend, append
         """
+        new_text = request_data["caller_id"]
+        mode = request_data["mode"]
+
         try:
+            if mode == "overwrite":
+                final_name = new_text
+            else:
+                res = self._ari.channels.getChannelVar(
+                    channelId=call_id,
+                    variable='CALLERID(name)'
+                )
+                current_name = res.get('value', '')
+
+                if mode == "prepend":
+                    final_name = f"{new_text}{current_name}"
+                elif mode == "append":
+                    final_name = f"{current_name}{new_text}"
+                else:
+                    final_name = new_text
+
             self._ari.channels.setChannelVar(
                 channelId=call_id,
-                variable='CALLERID(all)',
-                value=request_data["caller_id"]
+                variable='CALLERID(name)',
+                value=final_name
             )
+
             return {}, 204
+
         except Exception as e:
             return {'error': str(e)}, 500
